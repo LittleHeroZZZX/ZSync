6f2be5fa23d36b930da4783bd71b81abed2f0123
ZhouXin <zhou.xin2000@outlook.com>
2024-01-17 11:02:15 +0800

    修正action工作流。

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ZSync',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     url='https://github.com/LittleHeroZZZX/ZSync',
     license='MIT',
